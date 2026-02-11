b37a05d02e7b5f8339bf89319e96cb555d9fff5b
Ashutosh Kothiwala <ashutosh.kothiwala@gmail.com>
2026-02-12 00:59:25 +0530

    updated the implementation but unittests didn't succeed

--- llm/Sinusoidal-Positional-Embedding/sinusoidal_q7_initial_implementation.py
+++ llm/Sinusoidal-Positional-Embedding/sinusoidal_q7_initial_implementation.py
@@ -11,14 +11,22 @@ class SinusoidalPositionalEmbeddings(torch.nn.Module):
     def get_cos_wave(self, idx):
         return np.cos(idx/(10000**(np.arange(self.n_dim)/self.n_dim)))
 
-    def forward(self, idx):
-        if isinstance(idx, int):
-            print(idx)
-            if idx%2 == 0:
-                return self.get_sine_wave(idx)
-            elif idx%2 == 1:
-                return self.get_cos_wave(idx)
-        else:
-            sine_op = self.get_sine_wave(idx)*(1-np.arange(self.n_dim)%2)
-            cos_op = self.get_cos_wave(idx)*(np.arange(self.n_dim)%2)
-            return sine_op+cos_op
+    # INITIAL FAULTY IMPLEMENTATION
+    # def forward(self, idx):
+    #     if isinstance(idx, int):
+    #         print(idx)
+    #         if idx%2 == 0:
+    #             return self.get_sine_wave(idx)
+    #         elif idx%2 == 1:
+    #             return self.get_cos_wave(idx)
+    #     else:
+    #         sine_op = self.get_sine_wave(idx)*(1-np.arange(self.n_dim)%2)
+    #         cos_op = self.get_cos_wave(idx)*(np.arange(self.n_dim)%2)
+    #         return sine_op+cos_op
+
+    def forward(self, seq_len):
+        output = np.zeros(shape=(seq_len, self.n_dim))
+        idx = np.arange(start=0, stop=seq_len+1, step=2) # incrementing stop by 1 to cover odd sequence lengthes which would have been dropped due to stop=2
+        output[:,::2] = self.get_sine_wave(idx)
+        output[:,1::2] = self.get_cos_wave(idx)
+        return output[:, :seq_len]
